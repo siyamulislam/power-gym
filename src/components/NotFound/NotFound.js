83c1ce1fc4ad2540324b860abc45cc93033668db
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-04-04 23:49:45 +0600

    not found link to home

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,16 +1,19 @@
-import React from 'react'; 
+import React from 'react';
+import { Link } from 'react-router-dom';
 import NavSection from '../NavSection/NavSection';
 
 const NotFount = () => {
     return (
-        <div style={{backgroundColor:'black'}}>
+        <div style={{ backgroundColor: 'black' }}>
             <NavSection></NavSection>
-        
-          <h1>404 not found!</h1>
-          <h1>404 not found!</h1>
-          <h1>404 not found!</h1>
-       
-           
+            <h1>404 not found!</h1>
+            <div>
+                <p>Click to route to "/oops" which isn't a registered route:</p>
+                <Link to="/">Let's go back HOME </Link>
+            </div>
+            <h1>404 - Not Found!</h1>
+    <Link to="/">Go Home</Link>
+
         </div>
     );
 };
