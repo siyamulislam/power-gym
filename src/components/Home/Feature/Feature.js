564bd8b0ce40d0c689f45d717a62b209e60f74a2
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-04-05 23:52:38 +0600

    feature box template

---
+++ src/components/Home/Feature/Feature.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Feature.css'
+
+const Feature = () => {
+    return (
+        <div className='featureBox  '> 
+           <div className="featureDiv1 "></div>
+           <div className="featureDiv1 "></div>
+           <div className="featureDiv1 "></div>
+        </div>
+    );
+};
+
+export default Feature;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Feature from './Feature/Feature';
 import Header from './Header/Header';
 
 const Home = () => {
     return (
         <div> 
           <Header></Header>
+          <Feature></Feature>
         </div>
     );
 };
