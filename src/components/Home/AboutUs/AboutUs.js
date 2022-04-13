4971d7f57449a1b0868d5770ed6ae4c171c95d0e
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-04-13 23:51:46 +0600

    about us page content added

--- src/components/Home/AboutUs/AboutUs.js
+++ src/components/Home/AboutUs/AboutUs.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './AboutUs.css'
+import aboutImg from '../../../images/aboutUsLeft.jpg'
 
 const AboutUs = () => {
     return (
-        <div>
-            <h1 className='headCheck'>Clamp</h1>
-        </div>
+        <section className='aboutUS'>
+            <div className='row'>
+                <div className="col-md-6">
+                <img className='img-fluid' src={aboutImg} alt="" />
+                </div>
+                <div className="col-md-6">
+                <h1 className=''>About Us</h1>
+                <h2>WE ARE HERE TO DREAM! <br></br> <span>OUR TEAM IS HERE SURVE YOU</span></h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo assumenda eos voluptas. Quo porro expedita consequuntur hic, necessitatibus rerum quod dolores incidunt, odio doloribus accusantium.</p>
+                </div>
+                
+            </div>
+        </section>
     );
 };
 
