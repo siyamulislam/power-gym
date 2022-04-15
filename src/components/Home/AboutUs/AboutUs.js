3fdecc99f06678231ad3cfe77127f2c689312433
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-04-15 12:35:55 +0600

    about us done

--- src/components/Home/AboutUs/AboutUs.js
+++ src/components/Home/AboutUs/AboutUs.js
@@ -11,7 +11,7 @@ const AboutUs = () => {
                 </div>
                 <div className="col-md-6   right">
                 <h1 className=''>ABOUT US</h1>
-                <h2>WE ARE HERE TO DREAM! <br></br> <span>OUR TEAM IS HERE SURVE YOU</span></h2>
+                <h2>WE ARE HERE TO DREAM! <br></br> <span>OUR TEAM IS HERE SURVE YOU.</span></h2>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo assumenda eos voluptas. Quo porro expedita consequuntur hic, necessitatibus rerum quod dolores incidunt, odio doloribus accusantium.</p>
                 </div>
                 
