35764c82bdf0502b2e664643bf716d68f8cea2b3
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-04-20 01:30:13 +0600

    footer done with responsive

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,52 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBaby,  } from '@fortawesome/free-solid-svg-icons';
+import { faYoutube,faFacebookF,faInstagram,faTwitter,faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import React from 'react';
 import './Footer.css'
 const Footer = () => {
     return (
-       <section className='footer'>
-            <div className='row'>
-                <div className="col-md-3">11111111111</div>
-                <div className="col-md-2">22222222222</div>
-                <div className="col-md-2">334233333</div>
-                <div className="col-md-2">444444444444</div>
-                <div className="col-md-3">5555555555555555</div>
+        <section className='footer'>
+            <div className='footerBox'>
+                <div className='row '>
+                    <div className="col-xl-3 col-md-12 col-sm-12 ">
+                        <h3>POWER <span>GYM</span></h3>
+                    </div>
+                    <div className="col-xl-2 col-md-3 col-sm-6 ">
+                        <ul>
+                            <li><strong>Need Help?</strong></li>
+                            <li>Help Center</li>
+                            <li>Email Support</li>
+                            <li>Live Chat</li>
+                            <li>Gift Certificates</li>
+                            <li> Send Us Feedback</li>
+                        </ul>
+                    </div>
+                    <div className="col-xl-2 col-md-3 col-sm-6 ">
+                        <ul>
+                            <li><strong>Digital Resource</strong></li>
+                            <li>Articles</li>
+                            <li>E-books</li>
+                        </ul>
+                    </div>
+                    <div className="col-xl-2 col-md-3 col-sm-6 ">
+                        <ul>
+                            <li><strong>Connect With Us</strong></li>
+                            <li className='contactIcon'>
+                                <FontAwesomeIcon icon={faYoutube}/> <FontAwesomeIcon icon={faFacebookF}/>  <FontAwesomeIcon icon={faInstagram}/> <FontAwesomeIcon icon={faTwitter}/>  <FontAwesomeIcon icon={faWhatsapp}/> 
+                                </li>
+                            <li>Forum</li>
+                        </ul>
+                    </div>
+                    <div className="col-xl-3 col-md-3 col-sm-6 ">
+                    <ul>
+                            <li><strong>Join Our Newsletter</strong></li>
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium aut magnam ab.</li> 
+                        </ul>
+                    </div>
+                </div>
             </div>
-       </section>
+
+        </section>
     );
 };
 
