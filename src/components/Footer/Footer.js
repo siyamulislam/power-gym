d5f6111e5cc2480dde31076be61be4c9c1c57cb5
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-04-20 02:48:10 +0600

    footer social icon added/update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,8 +2,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBaby,  } from '@fortawesome/free-solid-svg-icons';
 import { faYoutube,faFacebookF,faInstagram,faTwitter,faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import React from 'react';
-import './Footer.css'
+import './Footer.css' 
 const Footer = () => {
+    var youtube = <a target="_blank" rel='noReferrer' href="//youtube.com/"><FontAwesomeIcon icon={faYoutube}/></a>
+    var facebook = <a target="_blank" rel='noReferrer' href="//facebook.com/"><FontAwesomeIcon icon={faFacebookF}/></a>
+    var instagram = <a target="_blank" rel='noReferrer' href="//instagram.com/"><FontAwesomeIcon icon={faInstagram}/></a>
+    var twitter = <a target="_blank" rel='noReferrer' href="//twitter.com/"><FontAwesomeIcon icon={faTwitter}/></a>
+    var whatsapp = <a target="_blank" rel='noReferrer' href="//whatsapp.com/"><FontAwesomeIcon icon={faWhatsapp}/></a>
+    
     return (
         <section className='footer'>
             <div className='footerBox'>
@@ -29,11 +35,9 @@ const Footer = () => {
                         </ul>
                     </div>
                     <div className="col-xl-2 col-md-3 col-sm-6 ">
-                        <ul>
+                        <ul> 
                             <li><strong>Connect With Us</strong></li>
-                            <li className='contactIcon'>
-                                <FontAwesomeIcon icon={faYoutube}/> <FontAwesomeIcon icon={faFacebookF}/>  <FontAwesomeIcon icon={faInstagram}/> <FontAwesomeIcon icon={faTwitter}/>  <FontAwesomeIcon icon={faWhatsapp}/> 
-                                </li>
+                            <li className='contactIcon'>{youtube} {facebook} {instagram} {twitter} {whatsapp}</li>
                             <li>Forum</li>
                         </ul>
                     </div>
