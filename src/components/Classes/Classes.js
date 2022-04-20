385dd8bc02a854a3d9f9c04e70a6396890ddcb9f
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-04-21 03:15:11 +0600

    card Designed

--- src/components/Classes/Classes.js
+++ src/components/Classes/Classes.js
@@ -2,13 +2,25 @@ import React from 'react';
 import Footer from '../Footer/Footer';
 import Header2 from '../Header2/Header2'; 
 import './Classes.css'
+import class1 from '../../images/class1.png'
+import ClassCard from './ClassCard/ClassCard';
 
 const Classes = () => {
+    const classDB=[
+        {title:"Psycho Training",   img:class1},
+        {title:"Self Defence",      img:class1},
+        {title:"Advanced Gym",      img:class1},
+        {title:"Cardio Training",   img:class1},
+        {title:"Psycho Training",   img:class1},
+        {title:"Strength Training", img:class1},
+
+    ]
     return (
         <div className='classes'>
             <Header2 heading={"Our Classes"}></Header2> 
-            <div className='classBody  '>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam doloribus beatae consequatur hic neque soluta tenetur vero obcaecati, optio dolores quia impedit velit corrupti modi pariatur sapiente eligendi facilis delonsequatur nulla. Veniam eligendi, praesentium consectetur nam libero, animi laboriosam reiciendis eum natus blanditiis porro dicta a!</p>
+            <div className='classBody '>
+                <ClassCard card={classDB[0]}></ClassCard>
+            
             </div>
             <Footer></Footer>
         </div>
