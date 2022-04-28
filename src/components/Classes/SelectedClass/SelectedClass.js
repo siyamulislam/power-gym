be8c414c82f509e149b3eaf86c64915daa2a6570
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-04-28 20:43:50 +0600

    selectedcard details 30%

--- src/components/Classes/SelectedClass/SelectedClass.js
+++ src/components/Classes/SelectedClass/SelectedClass.js
@@ -2,11 +2,18 @@ import React from 'react';
 import './SelectedClass.css'
 
 const SelectedClass = (props) => {
-    const {title}=props.card;
+    const {title,img}=props.card;
     // console.log(props); 
     return (
-        <div>
-            <h2>{title}</h2>
+        <div className='row selectedClass'>
+            <div className='col-md-7 left'>
+                <img src={img} alt="" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In quo fugit doloribus ducimus, atque itaque necessitatibus eius sequi, maxime voluptatum cum praesentium voluptatibus perferendis ullam nobis est saepe fuga commodi veritatis, quos ab sit tempore laboriosam. Voluptatem sint id est consectetur maiores praesentium vel nemo aperiam, autem nostrum recusandae ad.</p>
+            </div>
+            <div className='col-md-5 right'>
+                <h1> Class Secdule</h1>
+
+            </div>
         </div>
     );
 };
