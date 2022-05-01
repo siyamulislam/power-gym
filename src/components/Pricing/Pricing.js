7258da9576e28ab4ea713a3c719b2fc9819b27f7
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-05-01 15:03:32 +0600

    plan json added

--- src/components/Pricing/Pricing.js
+++ src/components/Pricing/Pricing.js
@@ -3,12 +3,38 @@ import Footer from '../Footer/Footer';
 import Header2 from '../Header2/Header2';
 import PriceCard from './PriceCard/PriceCard';
 import './pricing.css'
+import priceBg1 from '../../images/feature1.jpg'
+import priceBg2 from '../../images/feature2.jpg'
+import priceBg3 from '../../images/feature3.jpg'
 
 const Pricing = () => {
+    const priceList = [
+        {
+            duration: "Billed Monthly",
+            name: "Advanced Plan",
+            price: 140,
+            feature:["Mobile-Optimization","Best Fitness", "Free Equipment", "Lifetime Access", "Wifi Zone"],
+            img: priceBg1
+        },
+        {
+            duration: "Billed Monthly",
+            name: "Basic Plan",
+            price: 120,
+            feature:["Mobile-Optimization","Best Fitness", "Free Equipment", "Lifetime Access", "Wifi Zone"],
+            img: priceBg2
+        },
+        {
+            duration: "Billed Monthly",
+            name: "Beginners Plan",
+            price: 90,
+            feature:["Mobile-Optimization","Best Fitness", "Free Equipment", "Lifetime Access", "Wifi Zone"],
+            img: priceBg3
+        },
+    ]
     return (
         <div className='pricing'>
-             <Header2 heading={ "Pricing Plans" }></Header2>
-            <div  className='priceTopBody'>
+            <Header2 heading={"Pricing Plans"}></Header2>
+            <div className='priceTopBody'>
                 <h2>Choose The Offer <span>That Suits You</span></h2>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam illum autem, est quae natus. Voluptates, cupiditate.</p>
             </div>
