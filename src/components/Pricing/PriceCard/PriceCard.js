c56260e47fb2202fbb3d58eaaf437eeb0087d095
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-05-01 13:39:33 +0600

    priceTopBody added

---
+++ src/components/Pricing/PriceCard/PriceCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PriceCard.css'
+
+const PriceCard = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default PriceCard;
--- src/components/Pricing/Pricing.js
+++ src/components/Pricing/Pricing.js
@@ -7,10 +7,13 @@ const Pricing = () => {
     return (
         <div className='pricing'>
              <Header2 heading={ "Pricing Plans" }></Header2>
-            <h2>Pricing</h2>
-            <h2>Pricing</h2>
-            <h2>Pricing</h2>
-            <h2>Pricing</h2><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis vitae sint incidunt quasi, neque fuga sunt dolores? Quibusdam recusandae accusantium vero sequi eligendi itaque ab saepe ipsa, inventore ducimus tempora exercitationem et molestias cum, adipisci, tenetur maiores accusamus incidunt corporis! Ipsum est ut quae ducimus ex vitae sit hic velit repellendus, magnam quasi aut maiores ipsam debitis veritatis esse voluptates perspiciatis laboriosam! Eligendi nam ratione numquam, asperiores deserunt quibusdam esse magnam vero? Eius amet necessitatibus eveniet adipisci consequatur quidem maiores non aperiam incidunt rem! Odio, facilis iste. Blanditiis veniam alias laborum deserunt at doloremque maxime accusantium! Voluptatum facere unde, corporis quidem molestias quae laudantium iste dolorum, saepe omnis suscipit sed nesciunt dolor ratione doloribus quibusdam dignissimos et labore consequuntur nisi provident fugiat. Consectetur, vel suscipit. Quia perferendis dignissimos, perspiciatis dolores numquam laboriosam dolore alias reprehenderit sed, quas quo quidem! Totam quos sint, quam rerum et repudiandae atque veritatis quas ipsum ipsa, adipisci optio, cumque dignissimos voluptatem iste. Iure totam repellat unde atque placeat eos laudantium animi! Error incidunt deserunt eaque cumque sed labore vitae, libero dolorum iure, eveniet facilis neque id accusamus expedita omnis voluptas nemo ipsa quas pariatur! Ab laborum molestias dolorum! Nobis doloremque iure aut ad veniam eos illum dolorem nemo est enim officia architecto inventore nulla minus recusandae, placeat harum ab itaque pariatur facere officiis consectetur aperiam. Quia quae accusantium quas nulla dolorem ea officiis dolorum! Praesentium quibusdam quia id similique nostrum doloremque eos, dolorem excepturi qui sit natus soluta at, culpa aliquid dolor! Sed sit autem facilis alias voluptatum, neque quam tempore ratione expedita facere illo, quisquam repudiandae tenetur, omnis repellendus quos! Et voluptatum aliquam perspiciatis neque labore eum natus, consequuntur harum ad quod cum atque est excepturi nobis, cupiditate porro vel nemo fugit quia hic eos quisquam quis. Ad dicta voluptate, porro totam debitis sed?</p>
+            <div  className='priceTopBody'>
+                <h2>Choose The Offer <span>That Suits You</span></h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam illum autem, est quae natus. Voluptates, cupiditate.</p>
+            </div>
+            <div className='cardBody'>
+
+            </div>
             <Footer></Footer>
         </div>
     );
