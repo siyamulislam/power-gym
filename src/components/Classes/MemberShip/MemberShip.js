479df75c2fdd76122bd14b1b2393314ca797eed8
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-05-03 19:34:17 +0600

    mebership page added, @futureUpdate

--- src/components/Classes/MemberShip/MemberShip.js
+++ src/components/Classes/MemberShip/MemberShip.js
@@ -6,8 +6,8 @@ const MemberShip = () => {
     return (
         <div className="memberShip">
             <Header2 heading={"Your Gym MemberShip"} ></Header2>
-            <h3 >Need to develop from here.........</h3  >
-            <h3 >1. Personal Details <br />2. Bank Payment <br />3. Membership Created</h3   >
+            <h3 >Need to develop from here.....</h3  >
+            <h3 >1. Personal Details <br/>2. Bank Payment <br/>3. Membership Created</h3   >
             <h5>! wait for next update...</h5>
             <Footer></Footer>
         </div>
